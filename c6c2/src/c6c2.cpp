eab64dbf6a7d97cb073b423d2f09f6195edc6632
jessicah16871 <jessica.hong9@gmail.com>
2017-03-11 13:52:26 -0500

    added sorting

--- c6c2/src/c6c2.cpp
+++ c6c2/src/c6c2.cpp
@@ -15,6 +15,25 @@ struct Student{
 	int grade;
 };
 
+void sort(Student *array, int len){
+	const int length = len;
+
+	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
+		{
+			int biggestIndex = startIndex;
+
+			for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
+			{
+
+				if (array[currentIndex].grade > array[biggestIndex].grade)
+					biggestIndex = currentIndex;
+			}
+
+			std::swap(array[startIndex], array[biggestIndex]);
+		}
+
+}
+
 int main() {
 	std::cout << "Enter number of students: ";
 	int length;
@@ -29,6 +48,11 @@ int main() {
 		std::cin >> array[i].grade;
 	}
 
+	sort(array,length);
+
+	for(int i = 0; i < length; i++){
+		std::cout << array[i].name << " got the grade of " <<  array[i].grade << std::endl;
+	}
 
 	delete[] array;
 	array = 0;
